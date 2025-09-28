6967171b094e008125286ddc88426071124cf033
GitvermaShiva <shivamverma63970@gmail.com>
2025-09-28 16:13:52 +0530

    Created a shell script for the configurations.

---
+++ config.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+# PEM key location
+PEM="/mnt/d/Downloads/myLinux-key.pem"
+
+
+# EC2 connection details
+USER="ec2-user"
+HOST="ec2-13-234-186-130.ap-south-1.compute.amazonaws.com"
+
+
+# Paths
+LOCAL_PROJECT="/mnt/c/Users/india/Desktop/my_project/"
+REMOTE_PROJECT="/home/ec2-user/my_project/"
+REMOTE_LOGS="/home/ec2-user/logs/"
+LOCAL_LOGS="/mnt/c/Users/india/Desktop/logs/"
